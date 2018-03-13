0b8e334300f28255bd7a3d90e92d5a8d2e2dedb6
sotsys-159 <amit_patoliya@spaceotechnologies.com>
2018-03-13 18:07:31 +0530

    final commit

--- app/src/main/java/com/picker/MainActivity.java
+++ app/src/main/java/com/picker/MainActivity.java
@@ -1,107 +1,103 @@
 package com.picker;
 
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-
 import com.imagepickerview.ImagePickerUtils;
+import com.imagepickerview.ImagePickerUtils.ActionCallBack;
 import com.miguelbcr.ui.rx_paparazzo2.RxPaparazzo;
 import com.miguelbcr.ui.rx_paparazzo2.entities.FileData;
 import com.miguelbcr.ui.rx_paparazzo2.entities.Options;
-
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private ImagePickerUtils imgUtils;
+  private ImagePickerUtils imgUtils;
 
-    private Options options = null;
+  private Options options = null;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        imgUtils = new ImagePickerUtils.Builder(MainActivity.this)
-                .setShowRemove(false)
-                .build();
+    imgUtils = new ImagePickerUtils.Builder(MainActivity.this)
+        .setShowRemove(true)
+        .build();
 
-        //for Crop feature & get customization
-        options = new Options();
-        options.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
-        options.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
-        options.setActiveWidgetColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
-        options.setAspectRatio(1, 1);
-    }
+    //for Crop feature & get customization
+    options = new Options();
+    options.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+    options.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+    options.setActiveWidgetColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+    options.setAspectRatio(1, 1);
+  }
 
 
-    public void onClickPickture(View mView) {
+  public void onClickPickture(View mView) {
 
-        imgUtils.showDialog(new ImagePickerUtils.ActionCallBack() {
-            @Override
-            public void onCameraClick() {
+    imgUtils.showBottomSheet(getSupportFragmentManager(), new ActionCallBack() {
+      @Override
+      public void onCameraClick() {
+        callCamera();
+      }
 
-                callCamera();
+      @Override
+      public void onGallaryClick() {
+        callGallary();
+      }
+    });
 
-            }
 
-            @Override
-            public void onGallaryClick() {
+  }
 
-                callGallary();
-            }
+  private void callGallary() {
+
+    RxPaparazzo.single(this)
+        .crop(options)
+        .usingGallery()
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(response -> {
+          if (response.resultCode() != RESULT_OK) {
+            response.targetUI().showUserCanceled();
+            return;
+          }
+
+          response.targetUI().loadImage(response.data());
         });
 
 
-    }
+  }
 
-    private void callGallary() {
+  private void callCamera() {
 
-        RxPaparazzo.single(this)
-                .crop(options)
-                .usingGallery()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(response -> {
-                    if (response.resultCode() != RESULT_OK) {
-                        response.targetUI().showUserCanceled();
-                        return;
-                    }
+    RxPaparazzo.single(this)
+        .crop(options)
+        .usingCamera()
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(response -> {
+          if (response.resultCode() != RESULT_OK) {
+            response.targetUI().showUserCanceled();
+            return;
+          }
 
-                    response.targetUI().loadImage(response.data());
-                });
+          response.targetUI().loadImage(response.data());
+        });
+  }
 
+  private void showUserCanceled() {
 
-    }
+    //TODO When user cancel Action
+  }
 
-    private void callCamera() {
+  private void loadImage(FileData data) {
 
-        RxPaparazzo.single(this)
-                .crop(options)
-                .usingCamera()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(response -> {
-                    if (response.resultCode() != RESULT_OK) {
-                        response.targetUI().showUserCanceled();
-                        return;
-                    }
+    //TODO load file from file data
 
-                    response.targetUI().loadImage(response.data());
-                });
-    }
-
-    private void showUserCanceled() {
-
-        //TODO When user cancel Action
-    }
-
-    private void loadImage(FileData data) {
-
-        //TODO load file from file data
-
-    }
+  }
 }
